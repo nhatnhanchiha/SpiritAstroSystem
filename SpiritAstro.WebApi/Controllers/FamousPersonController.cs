f7e12489954d17993553df52e7fa749fca0b8f5b
Nguyen Van Bac <nhatnhanchiha@outlook.com>
2021-09-24 04:29:50 +0700

    fix loi chinh ta

--- SpiritAstro.WebApi/Controllers/FamousPersonController.cs
+++ SpiritAstro.WebApi/Controllers/FamousPersonController.cs
@@ -16,50 +16,55 @@ namespace SpiritAstro.WebApi.Controllers
     [ApiController]
     public class FamousPersonController : ControllerBase
     {
-        private readonly IFamousPersonService _famouspersonService;
+        private readonly IFamousPersonService _famousPersonService;
 
-        public FamousPersonController(IFamousPersonService famouspersonService)
+        public FamousPersonController(IFamousPersonService famousPersonService)
         {
-            _famouspersonService = famouspersonService;
+            _famousPersonService = famousPersonService;
         }
 
         [HttpGet("{id:long}")]
         public async Task<IActionResult> GetFamousPersonById(long id)
-    {
+        {
             try
             {
-                var famoupersonModel = await _famouspersonService.GetFamousPersonById(id);
-                return Ok(MyResponse<FamousPersonModel>.OkWithData(famoupersonModel));
+                var famousPersonModel = await _famousPersonService.GetFamousPersonById(id);
+                return Ok(MyResponse<FamousPersonModel>.OkWithData(famousPersonModel));
             }
             catch (ErrorResponse e)
             {
-                if(e.Error.Code == (int)HttpStatusCode.NotFound)
+                if (e.Error.Code == (int)HttpStatusCode.NotFound)
                 {
                     return Ok(MyResponse<object>.FailWithMessage(e.Error.Message));
                 }
 
                 return Ok(MyResponse<object>.FailWithMessage(e.Error.Message));
             }
-    }
+        }
+
         [HttpPost]
-        public async Task<IActionResult> CreateNewFamousPerson([FromBody] CreateFamousPersonRequest createFamousPersonRequest)
+        public async Task<IActionResult> CreateNewFamousPerson(
+            [FromBody] CreateFamousPersonRequest createFamousPersonRequest)
         {
             try
             {
-                var famouspersonId = await _famouspersonService.CreateFamousPerson(createFamousPersonRequest);
-                return Ok(MyResponse<long>.OkWithDetail(famouspersonId, "Created success famous person with id = {famouspersonId}"));
+                var famousPersonId = await _famousPersonService.CreateFamousPerson(createFamousPersonRequest);
+                return Ok(MyResponse<long>.OkWithDetail(famousPersonId,
+                    $"Created success famous person with id = {famousPersonId}"));
             }
             catch (ErrorResponse e)
             {
                 return Ok(MyResponse<object>.FailWithMessage(e.Error.Message));
             }
-            }
+        }
+
         [HttpPut("{id:long}")]
-        public async Task<IActionResult> UpdateFamousPerson(long id, [FromBody] UpdateFamousPersonRequest updateCategoryRequest)
+        public async Task<IActionResult> UpdateFamousPerson(long id,
+            [FromBody] UpdateFamousPersonRequest updateCategoryRequest)
         {
             try
             {
-                await _famouspersonService.UpdateFamousPerson(id, updateCategoryRequest);
+                await _famousPersonService.UpdateFamousPerson(id, updateCategoryRequest);
                 return Ok(MyResponse<object>.OkWithMessage("Updated success"));
             }
             catch (ErrorResponse e)
@@ -72,12 +77,13 @@ namespace SpiritAstro.WebApi.Controllers
                 return Ok(MyResponse<object>.FailWithMessage("Updated fail. " + e.Error.Message));
             }
         }
+
         [HttpDelete("{id:long}")]
         public async Task<IActionResult> DeleteFamousPerson(long id)
         {
             try
             {
-                await _famouspersonService.DeleteFamousPerson(id);
+                await _famousPersonService.DeleteFamousPerson(id);
                 return Ok(MyResponse<object>.OkWithMessage("Deleted success"));
             }
             catch (ErrorResponse e)
