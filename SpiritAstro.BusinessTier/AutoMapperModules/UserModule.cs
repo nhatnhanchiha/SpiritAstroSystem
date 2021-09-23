774662bd0264d42f2fb717cdc074471f2f97e531
Tín <tinntse140668@fpt.edu.vn>
2021-09-23 23:26:57 +0700

    second commit

--- SpiritAstro.BusinessTier/AutoMapperModules/UserModule.cs
+++ SpiritAstro.BusinessTier/AutoMapperModules/UserModule.cs
@@ -2,6 +2,7 @@
 using AutoMapper;
 using SpiritAstro.BusinessTier.Commons.Enums.User;
 using SpiritAstro.BusinessTier.Requests.User;
+using SpiritAstro.BusinessTier.ViewModels.Users;
 using SpiritAstro.DataTier.Models;
 
 namespace SpiritAstro.BusinessTier.AutoMapperModules
@@ -15,6 +16,8 @@ namespace SpiritAstro.BusinessTier.AutoMapperModules
                     opt.MapFrom(ignore => new List<UserRole> { new() { RoleId = "customer" } }))
                 .ForMember(des => des.StatusPayment, opt => opt.MapFrom(ignore => (int)UserStatusPayment.Inactive))
                 .ForMember(des => des.Status, opt => opt.MapFrom(ignore => (int)UserStatus.Active));
+            mc.CreateMap<DetailUserRequest, User>();
+            mc.CreateMap<User, UserModels>().ReverseMap();
         }
     }
 }
