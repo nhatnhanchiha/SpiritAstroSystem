094265211ea6357dc535dd8420d4ed98c9762041
Nguyen Van Bac <nhatnhanchiha@outlook.com>
2021-10-16 23:02:57 +0700

    :D

--- SpiritAstro.DataTier/Models/SpiritAstroContext.cs
+++ SpiritAstro.DataTier/Models/SpiritAstroContext.cs
@@ -45,7 +45,7 @@ namespace SpiritAstro.DataTier.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=54.169.107.173,1433;Initial Catalog=SpiritAstro;User ID=sa;Password=SE1416MyPassword!;Connection Timeout=30;");
+                optionsBuilder.UseSqlServer("Server=13.213.42.113,1433;Initial Catalog=SpiritAstro;User ID=sa;Password=BaoKhanh1;Connection Timeout=30;");
             }
         }
 
