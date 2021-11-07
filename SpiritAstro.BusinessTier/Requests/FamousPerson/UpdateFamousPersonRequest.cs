b490a4f79431454a0b75b392563701b702d3c3b8
Nham Duc Dat <datndse141132@fpt.edu.vn>
2021-11-07 21:31:27 +0700

    fix gender for famous person

--- SpiritAstro.BusinessTier/Requests/FamousPerson/UpdateFamousPersonRequest.cs
+++ SpiritAstro.BusinessTier/Requests/FamousPerson/UpdateFamousPersonRequest.cs
@@ -12,7 +12,7 @@ namespace SpiritAstro.BusinessTier.Requests.FamousPerson
         public string Description { get; set; }
         public int ZodiacId { get; set; }
         public string UrlImage { get; set; }
-        public short Gender { get; set; }
+        public bool? Gender { get; set; }
         public string DateOfBirth { get; set; }
     }
 }
